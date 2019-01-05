197ba452f95a8b4426413585df648ff0f16bcfc0
Le Hoai Thuong <lehoaithuong@lehoaithuong.com>
2019-01-05 19:58:12 +0700

    Made some changes

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const path = require('path')
 const app = express()
-const getCachedSensorReadings = require('./get-cached-sensor-readings.1')
+const getCachedSensorReadings = require('./get-cachedsensor-readings')
 /*
 Here, we are introduced to express middleware.
 Middleware is a fancy word to describe a set of actions
@@ -9,16 +9,17 @@ that have to take place before the request handler.
 In the below statement, we use the express.static
 middleware, and bind it to the /public route.
 */
-app.use('/public', express.static(path.join(__dirname,'public')))
+app.use('/public', express.static(path.join(__dirname,
+'public')))
 app.get('/temperature', function (req, res) {
-res.send('<strong>' + getCachedSensorReadings.getTemperature().toFixed(1) + '</strong>' + '°C')
+res.send('<strong>' +
+getCachedSensorReadings.getTemperature().toFixed(1) +
+'</strong>' + '°C')
 })
 app.get('/humidity', function (req, res) {
-res.send(getCachedSensorReadings.getHumidity().toFixed(1) + '%')
+res.send(getCachedSensorReadings.getHumidity
+().toFixed(1) + '%')
 })
-app.get('/public', function (req, res) {
-    res.sendFile(path.join(__dirname, 'index.html'))
-    })
 app.listen(3000, function () {
 console.log('Server listening on port 3000')
 })
