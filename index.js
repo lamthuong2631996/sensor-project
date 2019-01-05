69fa2637f516f23c7dfe160bf000c0d4e6591324
Le Hoai Thuong <lehoaithuong@lehoaithuong.com>
2019-01-05 22:32:48 +0700

    Made some changes

--- index.js
+++ index.js
@@ -1,25 +1,17 @@
 const express = require('express')
 const path = require('path')
 const app = express()
-const getCachedSensorReadings = require('./get-cachedsensor-readings')
-/*
-Here, we are introduced to express middleware.
-Middleware is a fancy word to describe a set of actions
-that have to take place before the request handler.
-In the below statement, we use the express.static
-middleware, and bind it to the /public route.
-*/
-app.use('/public', express.static(path.join(__dirname,
-'public')))
+const getCachedSensorReadings = require('./get-cached-sensor-readings')
+app.use('/public', express.static(path.join(__dirname, 'public')))
 app.get('/temperature', function (req, res) {
-res.send('<strong>' +
-getCachedSensorReadings.getTemperature().toFixed(1) +
-'</strong>')
+res.json({
+value:getCachedSensorReadings.getTemperature().toFixed(1)
+})
 })
 app.get('/humidity', function (req, res) {
-res.send('<strong>' +
-getCachedSensorReadings.getHumidity().toFixed(1) +
-'</strong>')
+res.json({
+value:getCachedSensorReadings.getHumidity().toFixed(1)
+})
 })
 app.listen(3000, function () {
 console.log('Server listening on port 3000')
--- script.js
+++ script.js
@@ -1,35 +1,107 @@
-/*
-We put the code for fetching temperature in its own
-function
-*/
+const temperatureCanvasCtx =
+document.getElementById('temperaturechart').getContext('2d')
+const temperatureChartConfig = {
+type: 'line',
+data: {
+labels: [],
+datasets: [{
+data: [],
+backgroundColor: 'rgba(255, 205, 210, 0.5)'
+}]
+},
+options: {
+legend: {
+display: false
+},
+responsive: true,
+maintainAspectRatio: false,
+scales: {
+yAxes: [{
+ticks: {
+suggestedMin: 10,
+suggestedMax: 40
+}
+}]
+}
+}
+}
+const temperatureChart = new Chart(temperatureCanvasCtx,
+temperatureChartConfig)
+const humidityCanvasCtx =
+document.getElementById('humiditychart').getContext('2d')
+const humidityChartConfig = {
+type: 'line',
+data: {
+labels: [],
+datasets: [{
+data: [],
+backgroundColor: 'rgba(197, 202, 233, 0.5)'
+}]
+},
+options: {
+legend: {
+display: false
+},
+responsive: true,
+maintainAspectRatio: false,
+scales: {
+yAxes: [{
+ticks: {
+suggestedMin: 30,
+suggestedMax: 90
+}
+}]
+}
+}
+}
+const humidityChart = new Chart(humidityCanvasCtx,
+humidityChartConfig)
+const pushData = (arr, value, maxLen) => {
+arr.push(value)
+if (arr.length > maxLen) {
+arr.shift()
+}
+}
+const humidityDisplay =
+document.getElementById('humidity-display')
+const temperatureDisplay =
+document.getElementById('temperature-display')
 const fetchTemperature = () => {
     fetch('/temperature')
     .then(results => {
-    return results.text()
+    return results.json()
     })
-    .then(text => {
-    const temperatureDisplay =
-    document.getElementById('temperature-display')
-    temperatureDisplay.innerHTML = text
+    .then(data => {
+    const now = new Date()
+    const timeNow = now.getHours() + ':' +
+    now.getMinutes() + ':' + now.getSeconds()
+    pushData(temperatureChartConfig.data.labels,
+    timeNow, 10)
+    pushData(temperatureChartConfig.data.datasets[0]
+    .data, data.value, 10)
+    temperatureChart.update()
+    temperatureDisplay.innerHTML = '<strong>' +
+    data.value + '</strong>'
     })
     }
-    /*
-    Make a similar function to fetch humidity
-    */
     const fetchHumidity = () => {
     fetch('/humidity')
     .then(results => {
-    return results.text()
+    return results.json()
     })
-    .then(text => {
-    const temperatureDisplay =
-    document.getElementById('humidity-display')
-    temperatureDisplay.innerHTML = text
+    .then(data => {
+    const now = new Date()
+    const timeNow = now.getHours() + ':' +
+    now.getMinutes() + ':' + now.getSeconds()
+    pushData(humidityChartConfig.data.labels, timeNow,
+    10)
+    pushData(humidityChartConfig.data.datasets[0].data,
+    data.value, 10)
+    humidityChart.update()
+    humidityDisplay.innerHTML = '<strong>' + data.value
+    + '</strong>'
     })
     }
-    /*
-    Call the above defined functions at regular intervals
-    */
     setInterval(() => {
     fetchTemperature()
     fetchHumidity()
