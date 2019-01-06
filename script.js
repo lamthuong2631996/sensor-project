cf27b3dee3e6a2f756a649a6654c4ebae3b40052
Le Hoai Thuong <lehoaithuong@lehoaithuong.com>
2019-01-06 10:15:22 +0700

    Made some changes

--- script.js
+++ script.js
@@ -1,90 +1,102 @@
-/**
-* Get the context of the temperature canvas element
-*/
-const temperatureCanvasCtx =
-document.getElementById('temperature-chart').getContext('2d')
-/**
-* Create a new chart on the context we just instantiated
-*/
-const temperatureChart = new Chart(temperatureCanvasCtx,
-{
-/**
-* Were going to show the ongoing temperature as a line
-chart
-*/
+const temperatureCanvasCtx = document.getElementById('temperature-chart').getContext('2d')
+const temperatureChartConfig = {
 type: 'line',
 data: {
-/**
-* This is mock data.
-* The labels, which will form our x-axis, are
-supposed to represent the time at which each reading was
-taken.
-* Finally, we add the dataset, whose data is an
-array of temperature values.
-* The background color is set to the same value as
-the earlier display, with some added transparency (which
-is why the 'rgba' representation is used)
-*/
-labels: ['10:30', '10:31', '10:32', '10:33'],
+labels: [],
 datasets: [{
-data: [12, 19, 23, 17],
+data: [],
 backgroundColor: 'rgba(255, 205, 210, 0.5)'
 }]
 },
 options: {
-/**
-* There is no need for a legend since there is only
-one dataset plotted
-* The 'responsive' and 'maintainAspectRatio' options
-are set so that the chart takes the width and height of
-the canvas, and does not set it on its own.
-*/
 legend: {
 display: false
 },
 responsive: true,
-maintainAspectRatio: false
+maintainAspectRatio: false,
+scales: {
+yAxes: [{
+ticks: {
+suggestedMin: 10,
+suggestedMax: 40
 }
-})    
+}]
+}
+}
+}
+const temperatureChart = new Chart(temperatureCanvasCtx, temperatureChartConfig)
+const humidityCanvasCtx = document.getElementById('humidity-chart').getContext('2d')
+const humidityChartConfig = {
+type: 'line',
+data: {
+labels: [],
+datasets: [{
+data: [],
+backgroundColor: 'rgba(197, 202, 233, 0.5)'
+}]
+},
+options: {
+legend: {
+display: false
+},
+responsive: true,
+maintainAspectRatio: false,
+scales: {
+yAxes: [{
+ticks: {
+suggestedMin: 30,
+suggestedMax: 90
+}
+}]
+}
+}
+}
+const humidityChart = new Chart(humidityCanvasCtx, humidityChartConfig)
+const pushData = (arr, value, maxLen) => {
+arr.push(value)
+if (arr.length > maxLen) {
+arr.shift()
+}
+}
+const humidityDisplay = document.getElementById('humidity-display')
+const temperatureDisplay = document.getElementById('temperature-display')
 const fetchTemperature = () => {
     fetch('/temperature')
     .then(results => {
-        /**
-* We want the results converted to json, so we use
-the fetch results' `json` method, which returns a promise
-with the JSON data instead of the string
-*/
-return results.json()
-})
-.then(data => {
-/**
-* In our server API route handler, the format of
-returned data was an object with a `value` property.
-* The value of the sensor reading is therefore
-available in `data.value`
-*/
-const temperatureDisplay =
-document.getElementById('temperature-display')
-/**
-* We add in the HTML tags on the front end script
-this time, leaving the backend to only provide us data
-*/
-temperatureDisplay.innerHTML = '<strong>' +
-data.value + '</strong>'
-})
-}
-/**
-* Repeat the same steps for the humidity API
-*/
-const fetchHumidity = () => {
-fetch('/humidity')
-.then(results => {
-return results.json()
-})
-.then(data => {
-const humidityDisplay =
-document.getElementById('humidity-display')
-humidityDisplay.innerHTML = '<strong>' + data.value
-+ '</strong>'
-})
-}
+    return results.json()
+    })
+    .then(data => {
+    const now = new Date()
+    const timeNow = now.getHours() + ':' +
+    now.getMinutes() + ':' + now.getSeconds()
+    pushData(temperatureChartConfig.data.labels,
+    timeNow, 10)
+    pushData(temperatureChartConfig.data.datasets[0]
+    .data, data.value, 10)
+    temperatureChart.update()
+    temperatureDisplay.innerHTML = '<strong>' +
+    data.value + '</strong>'
+    })
+    }
+    const fetchHumidity = () => {
+    fetch('/humidity')
+    .then(results => {
+    return results.json()
+    })
+    .then(data => {
+    const now = new Date()
+    const timeNow = now.getHours() + ':' +
+    now.getMinutes() + ':' + now.getSeconds()
+    pushData(humidityChartConfig.data.labels, timeNow,
+    10)
+    pushData(humidityChartConfig.data.datasets[0].data,
+    data.value, 10)
+    humidityChart.update()
+    humidityDisplay.innerHTML = '<strong>' + data.value
+    + '</strong>'
+    })
+    }
+    setInterval(() => {
+    fetchTemperature()
+    fetchHumidity()
+    }, 2000)
