002bd3975ae7c57ed9cd010fb97155c92c882a8e
Le Hoai Thuong <lehoaithuong@lehoaithuong.com>
2019-01-06 09:19:56 +0700

    Made some changes

--- script.js
+++ script.js
@@ -1,108 +1,48 @@
+/**
+* Get the context of the temperature canvas element
+*/
 const temperatureCanvasCtx =
-document.getElementById('temperaturechart').getContext('2d')
-const temperatureChartConfig = {
+document.getElementById('temperature-chart').getContext('2d')
+/**
+* Create a new chart on the context we just instantiated
+*/
+const temperatureChart = new Chart(temperatureCanvasCtx,
+{
+/**
+* Were going to show the ongoing temperature as a line
+chart
+*/
 type: 'line',
 data: {
-labels: [],
+/**
+* This is mock data.
+* The labels, which will form our x-axis, are
+supposed to represent the time at which each reading was
+taken.
+* Finally, we add the dataset, whose data is an
+array of temperature values.
+* The background color is set to the same value as
+the earlier display, with some added transparency (which
+is why the 'rgba' representation is used)
+*/
+labels: ['10:30', '10:31', '10:32', '10:33'],
 datasets: [{
-data: [],
+data: [12, 19, 23, 17],
 backgroundColor: 'rgba(255, 205, 210, 0.5)'
 }]
 },
 options: {
+/**
+* There is no need for a legend since there is only
+one dataset plotted
+* The 'responsive' and 'maintainAspectRatio' options
+are set so that the chart takes the width and height of
+the canvas, and does not set it on its own.
+*/
 legend: {
 display: false
 },
 responsive: true,
-maintainAspectRatio: false,
-scales: {
-yAxes: [{
-ticks: {
-suggestedMin: 10,
-suggestedMax: 40
+maintainAspectRatio: false
 }
-}]
-}
-}
-}
-const temperatureChart = new Chart(temperatureCanvasCtx,
-temperatureChartConfig)
-const humidityCanvasCtx =
-document.getElementById('humiditychart').getContext('2d')
-const humidityChartConfig = {
-type: 'line',
-data: {
-labels: [],
-datasets: [{
-data: [],
-backgroundColor: 'rgba(197, 202, 233, 0.5)'
-}]
-},
-options: {
-legend: {
-display: false
-},
-responsive: true,
-maintainAspectRatio: false,
-scales: {
-yAxes: [{
-ticks: {
-suggestedMin: 30,
-suggestedMax: 90
-}
-}]
-}
-}
-}
-const humidityChart = new Chart(humidityCanvasCtx,
-humidityChartConfig)
-const pushData = (arr, value, maxLen) => {
-arr.push(value)
-if (arr.length > maxLen) {
-arr.shift()
-}
-}
-const humidityDisplay =
-document.getElementById('humidity-display')
-const temperatureDisplay =
-document.getElementById('temperature-display')
-const fetchTemperature = () => {
-    fetch('/temperature')
-    .then(results => {
-    return results.json()
-    })
-    .then(data => {
-    const now = new Date()
-    const timeNow = now.getHours() + ':' +
-    now.getMinutes() + ':' + now.getSeconds()
-    pushData(temperatureChartConfig.data.labels,
-    timeNow, 10)
-    pushData(temperatureChartConfig.data.datasets[0]
-    .data, data.value, 10)
-    temperatureChart.update()
-    temperatureDisplay.innerHTML = '<strong>' +
-    data.value + '</strong>'
-    })
-    }
-    const fetchHumidity = () => {
-    fetch('/humidity')
-    .then(results => {
-    return results.json()
-    })
-    .then(data => {
-    const now = new Date()
-    const timeNow = now.getHours() + ':' +
-    now.getMinutes() + ':' + now.getSeconds()
-    pushData(humidityChartConfig.data.labels, timeNow,
-    10)
-    pushData(humidityChartConfig.data.datasets[0].data,
-    data.value, 10)
-    humidityChart.update()
-    humidityDisplay.innerHTML = '<strong>' + data.value
-    + '</strong>'
-    })
-    }
-    setInterval(() => {
-    fetchTemperature()
-    fetchHumidity()
-    }, 2000)
+})    
